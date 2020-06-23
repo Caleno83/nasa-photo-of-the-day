3c6ec0367a1e1b9b1c9ba9adfc5b3fcb75c65286
Jorge Jimenez <calenocam24@gmail.com>
2020-06-23 18:53:06 -0400

    step 4 done

--- src/components/NasaPage.js
+++ src/components/NasaPage.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import NasaTitle from "./NasaTitle";
+import NasaDate from "./NasaDate";
+import NasaImg from "./NasaImg";
+import NasaContent from "./NasaContent";
 
 const NasaPage = () => {
     const [data, setData] = useState([]);
@@ -8,6 +12,7 @@ const NasaPage = () => {
         axios.get("https://api.nasa.gov/planetary/apod?api_key=nEzWjDwQczu7kTai8rZrALpUBpDtqcrPBU0Ba6HK")
         .then( res => {
             console.log("The response for Nasa useEffect is :", res)
+            setData(res.data)
         })
 
         .catch( err => {
@@ -17,7 +22,6 @@ const NasaPage = () => {
 
     return (
             <div className="container">
-                
             </div>
     );
 }
